87df1a4b34c8e6f088249f5f40edcb36ebd4e65e
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-09-19 22:05:07 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -10,7 +10,5 @@ const refs = {
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  event.currentTarget.value === ''
-    ? (refs.output.textContent = 'незнакомец')
-    : (refs.output.textContent = event.currentTarget.value);
+  refs.output.textContent = event.currentTarget.value || 'незнакомец';
 }
